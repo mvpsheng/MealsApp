e3a2c55b46bf99a9ce5a7fabce4233f9757a4cb9
Guo Xusheng <51054870+mvpsheng@users.noreply.github.com>
2022-10-13 07:15:21 +0000

    添加meals组件显示api中的对象属性

---
+++ README.md
@@ -0,0 +1,2 @@
+## some learning issues
+what is a hook?  like: useState hook\ useContext hook
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,10 +2,20 @@ import {useGlobalContext} from '../context'
 
 
 const Meals = () => {
-  const context = useGlobalContext()
-  console.log(context)
-  return (
-    <h1>Meals</h1>
-  )
+  const {meals} = useGlobalContext();
+
+  return <section className="section-center">
+    {meals.map((singleMeal) => {
+    const {idMeal, strMeal : title, strMealThumb: image } = singleMeal
+    return <article key={idMeal} className="single-meal" >
+      <img src={image} style={{width:'200px'}} className="img"/>
+      <footer>
+        <h5>{title}</h5>      
+        <button className="like-btn">click me</button>
+      </footer>
+    </article>
+    })}
+  </section>
+  
 }
 export default Meals
