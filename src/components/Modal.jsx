58a472a6785eabd7f202ae6201525ac188f7df3d
Guo Xusheng <51054870+mvpsheng@users.noreply.github.com>
2022-10-15 04:32:37 +0000

    添加完成modal模块

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,22 @@
+import { useGlobalContext } from '../context'
 
 const Modal = () => {
-  return (
-    <h1>My Modal</h1>
-  )
+
+  const {selectedMeal, closeModal } = useGlobalContext()
+
+  const {strMealThumb:image,strMeal:title,strInstructions:text,strSource:source} = selectedMeal
+  
+  return <aside className='modal-overlay'>
+    <div className='modal-container'>
+      <img src={image} alt={title} className="img modal-img"/>
+      <div className='modal-content'>
+        <h4>{title}</h4>
+        <p>Cooking Instruction</p>
+        <p>{text}</p>
+        <a href={source} target="_blank">Original Source</a>
+      <button className="btn btn-hipster close-btn" onClick={closeModal}>Close</button>
+      </div>
+    </div>
+  </aside>
 }
 export default Modal
